82817916a371b4bd761af477932f1fb08bd832c5
Keylor Mora <keylor@orbelink.com>
2015-03-11 16:02:28 -0600

    proyectos

---
+++ sites/all/themes/vanderlaat/js/global.js
@@ -0,0 +1,32 @@
+
+jQuery(document).ready(function(){
+
+	
+
+
+	jQuery('#edit-field-categoria-proyecto-tid').change(function(){
+		jQuery('input#edit-submit-proyectos').click();
+	});
+	jQuery('#edit-field-servicio-tid').change(function(){
+		jQuery('input#edit-submit-proyectos').click();
+	});
+	jQuery('#edit-field-cliente-tid').change(function(){
+		jQuery('input#edit-submit-proyectos').click();
+	});
+});
+
+jQuery( document ).ajaxComplete(function() {
+
+	
+
+
+	jQuery('#edit-field-categoria-proyecto-tid').change(function(){
+		jQuery('input#edit-submit-proyectos').click();
+	});
+	jQuery('#edit-field-servicio-tid').change(function(){
+		jQuery('input#edit-submit-proyectos').click();
+	});
+	jQuery('#edit-field-cliente-tid').change(function(){
+		jQuery('input#edit-submit-proyectos').click();
+	});
+});
